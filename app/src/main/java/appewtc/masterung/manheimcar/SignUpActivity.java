52eb6bd12bf02a979a1bd4b8810766c64715c2f2
masterUNG <phrombutr@gmail.com>
2016-10-22 16:08:31 +0700

    Finish 22oct

--- app/src/main/java/appewtc/masterung/manheimcar/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/manheimcar/SignUpActivity.java
@@ -1,13 +1,57 @@
 package appewtc.masterung.manheimcar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString, userString, passwordString, imageString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+        //button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit Text
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") ||
+                        userString.equals("") ||
+                        passwordString.equals("")) {
+                    // Have Space
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this,
+                            R.drawable.bird48, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
+                    myAlert.myDialog();
+                }
+
+            }   // onClick
+        });
+
+
+    }   // Main Method
+
+}   // Main Class
