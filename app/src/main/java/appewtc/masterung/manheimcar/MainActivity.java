9e5f824d253060e3d1a39512a25aabce796e035f
masterUNG <phrombutr@gmail.com>
2016-10-24 10:33:34 +0700

    Read JSON finish

--- app/src/main/java/appewtc/masterung/manheimcar/MainActivity.java
+++ app/src/main/java/appewtc/masterung/manheimcar/MainActivity.java
@@ -1,11 +1,18 @@
 package appewtc.masterung.manheimcar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
                             R.drawable.nobita48, "Have Space",
                             "Please Fill All Every Blank");
                     myAlert.myDialog();
+                } else {
+                    // No Space
+                    MyConstant myConstant = new MyConstant();
+                    SynData synData = new SynData(MainActivity.this);
+                    synData.execute(myConstant.getUrlJSON(),
+                            myConstant.getTestTitle(),
+                            myConstant.getTestMessage());
                 }
 
 
@@ -57,5 +71,49 @@ public class MainActivity extends AppCompatActivity {
 
 
     }   // Main Method
+
+    private class SynData extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+        private String titleString, messageString;
+
+
+        public SynData(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                titleString = strings[1];
+                messageString = strings[2];
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("24octV1", "e doInBack ==> " + e.toString());
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("24octV1", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // SynData Class
+
+
 
 }   // Main Class   นี่คือ คลาสหลัก
